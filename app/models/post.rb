79a4a51e14c902553dbda0178486fa0d83638379
nemusagi <rinyoshida25@gmail.com>
2022-08-21 16:37:00 +0900

    モデルenumとバリデーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  include LiberalEnum
+
+  enum type: { :illust, :text }
+  liberal_enum :type
+  enum genre: { :original, :fanart }
+  liberal_enum :genre
+
+
+  validates :title, presence: true
+  validates :text, presence: true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
+  validates :type, inclusion: { in: Post.types.keys }
+  validates :type, inclusion: { in: Post.types.keys }
+  validates :is_published, inclusion: [true, false]
 end
